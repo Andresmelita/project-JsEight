00739cc519e1a5c0916271fa094020ac590844b4
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-19 22:19:53 -0300

    docs: shadow removed

--- components/Buttons/CathegoryOne.tsx
+++ components/Buttons/CathegoryOne.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-
 const CathegoryOne = () => {
+  return (
+    <button className="flex text-primary-gray h500-normal-13px border-primary-gray border-[1px] relative p-1 rounded-full w-[138px] h-[45px] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
+      Marcas y tiendas
+    </button>
+  );
+};
 
-    return (
-        <button className='flex text-primary-gray shadow-card h500-normal-13px border-primary-gray border-[1px] relative p-1 rounded-full w-[138px] h-[45px] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105'>
-            Marcas y tiendas
-        </button>
-    )
-}
-
-export default CathegoryOne
+export default CathegoryOne;
