37e8367e8b0175335aa2dc5f1b84109ddb4083a6
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-20 23:59:02 -0300

    feat: buttons added

--- components/Buttons/RightButton.tsx
+++ components/Buttons/RightButton.tsx
@@ -2,11 +2,11 @@ import { RiArrowRightLine } from 'react-icons/ri';
 
 const RightButton = () => {
   return (
-    <div className="flex bg-[#fff0] w-[59px] h-[59px] transition-all duration-300 hover:scale-105">
-      <button className="flex rounded-full border-primary-blue border-[4px] items-center justify-center w-full h-full">
+    <button className="flex bg-[#fff0] w-[59px] h-[59px] transition-all duration-300 hover:scale-105">
+      <div className="flex rounded-full border-primary-blue border-[4px] items-center justify-center w-full h-full">
         <RiArrowRightLine className="text-[45px] text-primary-blue" />
-      </button>
-    </div>
+      </div>
+    </button>
   );
 };
 
