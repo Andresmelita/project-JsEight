d22d86b8e07c65db6cfdcd2ce281865c2c994e50
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-24 17:30:47 -0300

    docs: profile page created

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -1,18 +1,50 @@
-import React, { ReactElement } from 'react'
-import Layout from '../../components/Layout'
-import NestedLayout from '../../components/NestedLayout'
+import { kMaxLength } from 'buffer';
+import Image from 'next/image';
+import { ReactElement, useEffect, useState } from 'react';
+import Cathegory from '../../components/Buttons/Cathegory';
+import Layout from '../../components/Layout';
+import NestedLayout from '../../components/NestedLayout';
 
+export default function ProfilePage() {
+  const [data, setData] = useState(['']);
 
-export default function profilePage(){
+  useEffect(() => {
+    Cathegories();
+  }, []);
+
+  const Cathegories = () => {
+    setData(['Mis votos', 'Mis publicaciones']);
+  };
+
   return (
-    <p>hello world from Profile</p>
-  )  
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex bg-primary-blue h-[129px] justify-center items-end w-[100vw]">
+        <div className="max-h-[117px] max-w-[117px] bg-transparent mb-[-60px] z-50">
+          <Image
+            src="/images/profileImage.png"
+            alt=""
+            width={kMaxLength}
+            height={kMaxLength}
+            className="flex rounded-full"
+          />
+        </div>
+      </div>
+      <div className="flex gap-[13px] w-max h-[45px] mb-[82px] mt-[82px]">
+        <div className="flex w-[100%]">
+          <Cathegory Cathegories={data[0]} />
+        </div>
+        <div className="flex w-[100%]">
+          <Cathegory Cathegories={data[1]} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-profilePage.getLayout = function (page: ReactElement) {
+ProfilePage.getLayout = function (page: ReactElement) {
   return (
     <Layout>
       <NestedLayout>{page}</NestedLayout>
     </Layout>
-  )
-}
+  );
+};
