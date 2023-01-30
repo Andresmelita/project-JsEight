c9188dc7be1c4096429b67e410c751c4eb27f650
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-30 20:57:59 -0300

    feat: adjust slider and elements to container

--- components/Buttons/Cathegory.tsx
+++ components/Buttons/Cathegory.tsx
@@ -1,6 +1,6 @@
 const Cathegory = ({ Cathegories }: { Cathegories: string }) => {
   return (
-    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-2 border-primary-gray p-4 pt-2 pb-2 rounded-[23px] w-fit max-h-[30px] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
+    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-[1px] border-primary-gray p-4 pt-2 pb-2 rounded-[23px] w-fit h-[100%] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
       {Cathegories}
     </button>
   );
