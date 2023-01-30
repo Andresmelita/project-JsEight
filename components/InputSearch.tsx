51383f20fc0541e20cc28689c4e15d04baf9f0f0
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-01-30 20:58:38 -0300

    feat: border's inputsearch added

--- components/InputSearch.tsx
+++ components/InputSearch.tsx
@@ -2,7 +2,7 @@ import { AiOutlineSearch } from 'react-icons/ai';
 
 const InputSearch = () => {
   return (
-    <div className="wrapper w-[100%] max-w-[465px] m-1 h400-normal-13px h-[46px]">
+    <div className="wrapper w-[100%] max-w-[465px] m-1 h400-normal-13px h-[46px] border-[1px] border-primary-gray rounded-full">
       <div className="searchBar w-[100%] flex flex-row items-center rounded-[23px] bg-white h-[100%]">
         <input
           id="searchInput"
