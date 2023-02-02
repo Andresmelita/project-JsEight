a50079532a589d5ce16bab40e896acc3491092bd
Gilbert3052 <gilbertfernandez3052@gmail.com>
2023-02-02 18:41:57 -0400

    feat: buttons padding

--- components/Buttons/Cathegory.tsx
+++ components/Buttons/Cathegory.tsx
@@ -1,6 +1,6 @@
 const Cathegory = ({ Cathegories }: { Cathegories: string }) => {
   return (
-    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-[1px]  border-primary-gray p-[16px] md:p-[19px] pt-[14px] pb-[14px] rounded-[23px] w-[100%] h-[100%] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
+    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-[1px]  border-primary-gray p-[13px] md:p-[19px] pt-[14px] pb-[14px] rounded-[23px] w-[100%] h-[100%] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
       {Cathegories}
     </button>
   );
