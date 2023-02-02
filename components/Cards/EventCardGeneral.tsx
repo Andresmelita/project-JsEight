d47365f7cd76af8d6d02e3006ab3ec9d219b421e
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-02-02 18:44:00 -0300

    feat: eventCard fixed

--- components/Cards/EventCardGeneral.tsx
+++ components/Cards/EventCardGeneral.tsx
@@ -31,7 +31,7 @@ const EventCardGeneral = ({
           alt="cover"
         />
       </div>
-      <div className="BodyContent pl-[23px] pr-[23px] mt-4 h-52 flex flex-col gap-4 text-start">
+      <div className="BodyContent pl-[23px] pr-[23px] mt-[15px] pb-[30px] h-52 content-between grid gap-4 text-start">
         <div className="content__event h-26 flex flex-col gap-2">
           <Link href="/details/1">
             <div className="content__tittle h600-medium--20px text-[#1A1E2E]">
@@ -44,15 +44,17 @@ const EventCardGeneral = ({
             </div>
           </Link>
         </div>
-        <div className="content__page h500-medium-14px text-[#1B4DB1]">
-          <a href="">{link}</a>
-        </div>
-        <div className="content__votes flex justify-left items-center gap-1 h-4">
-          <div className="votes__icon">
-            <PersonIcon></PersonIcon>
+        <div className="flex flex-col">
+          <div className="content__page h500-medium-14px text-[#1B4DB1] pb-[16px]">
+            <a href="">{link}</a>
           </div>
-          <div className="votes__quantity text-[#1A1E2E] h500-medium-14px">
-            <p>{votes}</p>
+          <div className="content__votes flex justify-left items-center gap-1 h-4">
+            <div className="votes__icon">
+              <PersonIcon></PersonIcon>
+            </div>
+            <div className="votes__quantity text-[#1A1E2E] h500-medium-14px">
+              <p>{votes}</p>
+            </div>
           </div>
         </div>
       </div>
