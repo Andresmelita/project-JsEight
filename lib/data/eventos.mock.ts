1d4ee7fbd6c6929b566e4abf9d5f7f88ec5d3c5c
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-02-03 17:58:45 -0300

    feat: id added

--- lib/data/eventos.mock.ts
+++ lib/data/eventos.mock.ts
@@ -1,5 +1,6 @@
 export const eventos = [
   {
+    id: 1,
     cover: 'https://images2.imgbox.com/59/e9/XjEAZTU9_o.png',
     title: 'Concierto de Lady Gaga',
     description: 'El concierto con la temática de Lady Gaga en Las Vegas',
@@ -8,6 +9,7 @@ export const eventos = [
     like: true,
   },
   {
+    id: 2,
     cover: 'https://images2.imgbox.com/49/62/pszCAGzR_o.png',
     title: 'Concierto de BTS',
     description: 'Grupo musical coreano.',
@@ -16,6 +18,7 @@ export const eventos = [
     like: true,
   },
   {
+    id: 3,
     cover: 'https://images2.imgbox.com/2c/d7/nfqWmOvu_o.png',
     title: 'Tienda de ropa femenina ZARA',
     description: 'Tienda de ropa.',
@@ -24,6 +27,7 @@ export const eventos = [
     like: true,
   },
   {
+    id: 4,
     cover: 'https://thumbs2.imgbox.com/47/47/dvCfyApV_t.png',
     title: 'Hotel Selina',
     description:
@@ -33,6 +37,7 @@ export const eventos = [
     like: true,
   },
   {
+    id: 5,
     cover: 'https://images2.imgbox.com/75/7e/oqt5nojY_o.png',
     title: 'Marca Ecoalf',
     description:
