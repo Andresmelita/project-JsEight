f6f55fb82319f3c5f63a199ba5ee7332ada79bcd
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-02-04 00:05:20 -0300

    feat: changed

--- lib/services/auth.services.ts
+++ lib/services/auth.services.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 function login(data: { email: string; password: string }) {
-  return axios.post(
-    'https://paracuando.symphonik.site/api/v1/auth/login',
-    data
-  );
+  return axios.post('', data);
 }
 
 export { login };
