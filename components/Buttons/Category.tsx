2c4eeb65135d2fd1c7fd54c8b57e802febd4bb40
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-02-10 05:48:07 -0300

    feat: button category modified

--- components/Buttons/Category.tsx
+++ components/Buttons/Category.tsx
@@ -1,6 +1,6 @@
 const Category = ({ Categories }: { Categories: string }) => {
   return (
-    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-[1px]  border-primary-gray p-[13px] pt-[0px] pb-[0px] rounded-[23px] w-[100%] h-[100%] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
+    <button className="flex whitespace-pre text-primary-gray h500-normal-13px border-[1px]  border-primary-gray pr-[17px] pl-[17px] pt-[13px] pb-[13px] rounded-[23px] w-[100%] h-[100%] bg-[#fff] justify-center items-center hover:text-primary-grayDark transition-all duration-300 hover:scale-105">
       {Categories}
     </button>
   );
