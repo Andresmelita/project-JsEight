e62a367adee2739be448442b4a3b5a0b4c48af39
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-02-14 03:36:02 -0300

    feat: input search fixed

--- components/InputSearch.tsx
+++ components/InputSearch.tsx
@@ -11,7 +11,7 @@ const InputSearch = () => {
       }}
     >
       {(formikProps) => (
-        <Form className="wrapper w-[100%] max-w-[465px] m-1 h400-normal-13px h-[46px] border-[1px] border-primary-gray rounded-full">
+        <Form className="text-primary-grayDark wrapper w-[100%] max-w-[465px] m-1 h400-normal-13px h-[46px] border-[1px] border-primary-gray rounded-full">
           <div className="searchBar w-[100%] flex flex-row items-center rounded-[23px] bg-white h-[100%]">
             <Field
               id="searchInput"
